32b402836d27bf56dc029df89e46d26cc9021dac
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-20 16:53:43 -0300

    feat (beers-list): implement beers list module interactor

---
+++ beer-store/Modules/BeersList/Interactor/BeersListInteractor.swift
@@ -0,0 +1,32 @@
+//
+//  BeerListInteractor.swift
+//  beer-store
+//
+//  Created by Augusto Falcão on 12/20/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import Foundation
+
+class BeersListInteractor: BeersListUseCase {
+    weak var output: BeersListInteractorOutput?
+
+    func fetchBeersList() {
+        let url = URL(fileURLWithPath: "https://api.punkapi.com/v2/beers")
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                self.output?.beersListFetchFailed()
+            } else {
+                guard let data = data else { return }
+
+                do {
+                    let beersList = try JSONDecoder().decode([Beer].self, from: data)
+                    self.output?.beersListFetched(beersList)
+                } catch let error {
+                    print("Serializtion Error: \(error)")
+                }
+            }
+        }
+    }
+}
