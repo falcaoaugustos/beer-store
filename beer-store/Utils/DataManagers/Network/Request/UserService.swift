3c8549ac38762bcf91e4f159f7b84ab17ca6824f
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-22 03:37:25 -0300

    feat (network): implement user service request

---
+++ beer-store/Utils/DataManagers/Network/Request/UserService.swift
@@ -0,0 +1,46 @@
+//
+//  UserService.swift
+//  beer-store
+//
+//  Created by Augusto Falcão on 12/22/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import Foundation
+
+enum UserService: ServiceRequest {
+    case getBeersList
+
+    var path: String {
+        switch self {
+        case .getBeersList:
+            return "/beers"
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .getBeersList:
+            return .get
+    }
+
+    var body: Data? {
+        switch self {
+        case .getBeersList:
+            return nil
+        }
+    }
+
+    var header: [String: String]? {
+        switch self {
+        case .getBeersList:
+            return nil
+        }
+    }
+
+    var dataType: ResponseType {
+        switch self {
+        case .getBeersList:
+            return .json
+        }
+    }
+}
