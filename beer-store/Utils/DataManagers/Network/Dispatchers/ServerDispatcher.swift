a4d271543bdded3ea1ea880a431c59c525609377
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-22 04:34:53 -0300

    feat (network): implement server dispatcher

---
+++ beer-store/Utils/DataManagers/Network/Dispatchers/ServerDispatcher.swift
@@ -0,0 +1,50 @@
+//
+//  ServerDispatcher.swift
+//  beer-store
+//
+//  Created by Augusto Falcão on 12/22/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import Foundation
+
+protocol ServerDispatcherDelegate {
+    func didReceiveRequestResponseData(_ data: Data?)
+}
+
+class ServerDispatcher: RequestDispatcher {
+
+    private var baseURL: String
+    var delegate: ServerDispatcherDelegate? = nil
+
+    required init(baseURL: String) {
+        self.baseURL = baseURL
+    }
+
+    func execute(request: ServiceRequest) {
+        let requestURL = prepareURLRequest(for: request)
+
+        switch request.dataType {
+        case .json:
+            var req = URLRequest(url: requestURL)
+            req.httpMethod = request.method.rawValue
+            req.httpBody = request.body
+            req.allHTTPHeaderFields = request.header
+
+            URLSession.shared.dataTask(with: req) { (data, resp, error) in
+                let serverResponse = Response((data: data, resp: resp as? HTTPURLResponse, error: error), for: request)
+
+                switch serverResponse {
+                case let .error(httpErrorCode, error):
+                    print("Error \(httpErrorCode ?? 400): \(error.localizedDescription)")
+                case .json(let data):
+                    self.delegate?.didReceiveRequestResponseData(data)
+                }
+            }.resume()
+        }
+    }
+
+    private func prepareURLRequest(for request: ServiceRequest) -> URL {
+        return URL(string: "\(baseURL)\(request.path)")!
+    }
+}
