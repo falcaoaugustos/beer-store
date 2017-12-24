be734029c186fea151de3c3d0da9088deb9e9316
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-24 18:58:40 -0300

    feat (data manager): create json manager protocol

---
+++ beer-store/Utils/DataManagers/Local/Protocols/JSONManager.swift
@@ -0,0 +1,57 @@
+//
+//  JSONManager.swift
+//  beer-store
+//
+//  Created by Augusto Falcão on 12/24/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONManager {
+    static var name: String { get }
+
+    static func loadData()
+    static func saveData()
+}
+
+extension JSONManager {
+    static var sourcePath: String? {
+        return Bundle.main.path(forResource: self.name, ofType: "json")
+    }
+
+    static var destinyPath: String? {
+        let dir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        return "\(dir[0])/\(self.name).json"
+    }
+
+    static func saveData() {}
+
+    static func readJSON<T: Decodable>() -> [T] {
+        var array: [T] = []
+
+        guard let source = self.sourcePath, let destiny = self.destinyPath else {
+                print("[ERROR] source and/or destiny paths invalid")
+                return []
+        }
+
+        if !(FileManager.default.fileExists(atPath: destiny)) {
+            do {
+                try FileManager.default.copyItem(atPath: source, toPath: destiny)
+            } catch let error {
+                print("[ERROR] \(error.localizedDescription)")
+            }
+        }
+
+        if FileManager.default.fileExists(atPath: destiny) {
+            do {
+                let data = try NSData(contentsOfFile: source) as Data
+                array = try JSONDecoder().decode([T].self, from: data)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+
+        return array
+    }
+}
