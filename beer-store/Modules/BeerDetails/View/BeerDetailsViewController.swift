9f24dbe11a6dc3a56449d03a5084c3bab499f744
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-24 03:40:41 -0300

    refactor (beer details): update beer details layout

--- beer-store/Modules/BeerDetails/View/BeerDetailsViewController.swift
+++ beer-store/Modules/BeerDetails/View/BeerDetailsViewController.swift
@@ -13,7 +13,9 @@ class BeerDetailsViewController: UIViewController, BeerDetailsView {
     var presenter: BeerDetailsPresentation!
 
     @IBOutlet var beerImageView: UIImageView!
-    @IBOutlet var detailsTextView: UITextView!
+    @IBOutlet var beerNameLabel: UILabel!
+    @IBOutlet var beerTaglineLabel: UILabel!
+    @IBOutlet var beerDescriptionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +24,20 @@ class BeerDetailsViewController: UIViewController, BeerDetailsView {
 
     func showDetails(forBeer beer: Beer) {
         beerImageView.updateImageFrom(string: beer.image_url)
-        
-        detailsTextView.text = """
-        \(beer.name)
-        \(beer.tagline)
 
-        \(beer.description)
-        """
+        beerNameLabel.font = beerNameLabel.font.withSize(UIScreen.main.bounds.height / 25)
+        beerNameLabel.lineBreakMode = .byWordWrapping
+        beerNameLabel.numberOfLines = 0
+        beerNameLabel.text = beer.name
+
+        beerTaglineLabel.font = beerTaglineLabel.font.withSize(UIScreen.main.bounds.height / 38)
+        beerTaglineLabel.lineBreakMode = .byWordWrapping
+        beerTaglineLabel.numberOfLines = 0
+        beerTaglineLabel.text = beer.tagline
+
+        beerDescriptionLabel.font = beerDescriptionLabel.font.withSize(UIScreen.main.bounds.height / 50)
+        beerDescriptionLabel.lineBreakMode = .byWordWrapping
+        beerDescriptionLabel.numberOfLines = 0
+        beerDescriptionLabel.text = beer.description
     }
 }
