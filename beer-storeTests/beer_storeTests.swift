f7e282020a8f4b8078d90c0aa5cd71bc445aa49e
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-27 21:27:37 -0300

    chore (XCode): add XCode tests target

---
+++ beer-storeTests/beer_storeTests.swift
@@ -0,0 +1,35 @@
+//
+//  beer_storeTests.swift
+//  beer-storeTests
+//
+//  Created by Augusto Falcão on 12/27/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import XCTest
+
+class beer_storeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
