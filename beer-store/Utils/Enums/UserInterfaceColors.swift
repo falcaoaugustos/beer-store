53e962ccd16df78726a4995bc2b9f9371ff0699c
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-29 03:27:23 -0300

    feat (colors): implement an interface to user interface colors

---
+++ beer-store/Utils/Enums/UserInterfaceColors.swift
@@ -0,0 +1,14 @@
+//
+//  UserInterfaceColors.swift
+//  beer-store
+//
+//  Created by Augusto Falcão on 12/29/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import UIKit
+
+enum UserInterfaceColor: String {
+    case orange
+    case clear
+}
