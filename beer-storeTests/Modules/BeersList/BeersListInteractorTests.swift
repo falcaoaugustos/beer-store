7ae85db81d271dfa9407f06485c6a8fce390c186
Augusto Falcao <falcaoaugustos@gmail.com>
2017-12-29 22:21:58 -0300

    feat (tests): implement beers list interactor tests

---
+++ beer-storeTests/Modules/BeersList/BeersListInteractorTests.swift
@@ -0,0 +1,25 @@
+//
+//  BeersListInteractorTests.swift
+//  beer-storeTests
+//
+//  Created by Augusto Falcão on 12/29/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import XCTest
+@testable import beer_store
+
+class BeersListInteractorTests: XCTestCase {
+    func testAddBookmarkedBeer() {
+        let interactor = BeersListInteractor()
+        let result = interactor.bookmarkBeer(Beer(name: "", tagline: "", description: "", image_url: ""))
+        XCTAssert(result)
+    }
+
+    func testAddAndRemoveBookmarkedBeer() {
+        let interactor = BeersListInteractor()
+        interactor.bookmarkBeer(Beer(name: "", tagline: "", description: "", image_url: ""))
+        let result = interactor.bookmarkBeer(Beer(name: "", tagline: "", description: "", image_url: ""))
+        XCTAssertFalse(result)
+    }
+}
